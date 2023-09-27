ae596fc419fc6bf7c591993c315d0d0455cef7a9
Nicholas Matos <nicholas.matos@versed.net.br>
2023-09-27 16:16:34 -0300

    New Programs

---
+++ 99-MiniProjects/CPFValidator/cpf.asm
@@ -0,0 +1,137 @@
+option casemap:none
+
+nl  = 10
+nll = 0
+maxLen = 512
+
+.const
+    ttlStr     byte  "CPF Validator",nll
+    promptStr  byte  "Type your CPF: ",nll
+    isValid    byte  "Your CPF is valid! :D",nl,nll
+    isInvalid  byte  "Your CPF is invalid! :c",nl,nll
+    divis      dword   0bh
+    mult       dword   0ah
+
+.data
+    input byte maxLen dup (?)
+
+.code
+    externdef printf:proc
+	externdef readLine:proc
+	externdef atoi:proc
+    externdef puts:proc
+
+    public getTitle
+    getTitle proc
+        lea rax, ttlStr
+        ret
+    getTitle endp
+
+    valCPF proc
+
+        sub rsp, 56
+
+        push rcx
+
+        xor rax, rax
+        mov r8, 0ah
+        xor r9, r9
+
+    loop1: mov al, [rcx]
+        sub al, 30h
+        imul r8
+
+        inc rcx
+        dec r8
+
+        add r9, rax
+
+        cmp r8, 2
+        jae loop1
+        
+        mov rax, r9
+        imul mult
+        idiv divis
+
+        xor rax, rax
+        mov al, [rcx]
+        sub al, 30h
+        cmp al, dl
+        jne invalidCpf
+
+        pop rcx
+
+        xor rax, rax
+        mov r8, 0bh
+        xor r9, r9
+
+    loop2: mov al, [rcx]
+        sub al, 30h
+        imul r8
+
+        inc rcx
+        dec r8
+
+        add r9, rax
+
+        cmp r8, 2
+        jae loop2
+
+        mov rax, r9
+        imul mult
+        idiv divis
+        
+        xor rax, rax
+        mov al, [rcx]
+        sub al, 30h
+        cmp al, dl
+        jne invalidCpf
+
+        lea rcx, isValid
+        call printf
+        jmp allDone
+
+invalidCpf: 
+        pop rcx
+        lea rcx, isInvalid
+        call printf
+
+        mov rax, -1
+        
+    allDone: add rsp, 56
+        ret
+
+    valCPF endp
+
+    readCPF proc
+        sub rsp, 56
+
+        ; prompt has been passed per parameter
+        call printf
+
+        lea rcx, input
+        mov rdx, maxLen
+        call readLine
+
+        cmp rax, nll
+        je badInput
+
+        lea rcx, input
+        call valCPF
+
+    badInput: add rsp, 56
+        ret
+    readCPF endp
+
+    public asmMain
+
+    asmMain proc
+        sub rsp, 38h
+
+        lea rcx, promptStr
+        call readCPF
+
+        add rsp, 38h
+        ret
+    asmMain endp
+            end
---
+++ Readme.md
@@ -0,0 +1,1 @@
+** Codigos em Assembly e C++ **
